f3f6c1054192e8761582c6e232d57fa3965de768
rainbrry <ade.usman403@gmail.com>
2022-10-31 11:24:58 +0700

    add reset password

--- src/app/controllers/users.controller.js
+++ src/app/controllers/users.controller.js
@@ -1,5 +1,5 @@
 import User from "#model/user.model";
-import { encryptPassword } from "#helper/EncryptPassword";
+import { encryptPassword, verifyPassword } from "#helper/EncryptPassword";
 
 // Controller for the user routes
 
@@ -53,27 +53,44 @@ const UsersController = {
 		await User.findByIdAndUpdate(req.params.id, req.body, {
 			new: true,
 		})
-			.then((user) => res.status(200).json(user))
+			.then((user) =>
+				res
+					.status(200)
+					.json({ message: "User updated successfully", data: user })
+			)
 			.catch((err) => res.status(500).json(err));
 	},
 
 	// Delete a user by id
 	destroy: async (req, res) => {
 		await User.findByIdAndDelete(req.params.id)
-			.then((user) => res.status(200).json(user))
+			.then(() =>
+				res.status(200).json({ message: "User deleted successfully" })
+			)
 			.catch((err) => res.status(500).json(err));
 	},
 
 	// Reset password
 	resetPassword: async (req, res) => {
+		const { currentPassword, newPassword, confirmPassword } = req.body;
+
 		await User.findById(req.params.id)
 			.select("+password")
 			.then((user) => {
-				if (req.body.currentPassword === user.password) {
-					user.password = req.body.newPassword;
-					user.save();
-					res.status(200).json("Success");
+				// check password
+				const validPassword = verifyPassword(user.password);
+
+				if (currentPassword !== validPassword) {
+					return res.status(400).json({ message: "Invalid password" });
 				}
+
+				if (newPassword !== confirmPassword) {
+					return res.status(400).json({ message: "Passwords do not match" });
+				}
+
+				user.password = encryptPassword(newPassword);
+				user.save();
+				return res.status(200).json({ message: "Password reset successfully" });
 			})
 			.catch((err) => res.status(500).json(err));
 	},
--- src/app/routes/api.route.js
+++ src/app/routes/api.route.js
@@ -7,10 +7,12 @@ const route = express.Router();
 route.post("/login", AuthController.login);
 
 route.get("/", (req, res) => res.send("hello"));
+
 route.get("/users", UsersController.index);
 route.get("/user/:id", UsersController.show);
 route.post("/user", UsersController.store);
-
-route.post("/tes", (req, res) => console.log(req.body));
+route.patch("/user/:id", UsersController.update);
+route.put("/reset-password/:id", UsersController.resetPassword);
+route.delete("/user/:id", UsersController.destroy);
 
 export default route;
